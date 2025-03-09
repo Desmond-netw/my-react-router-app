d1c48e9b50a25bb88f501a23b92270f1909f968e
Desmond-netw <dsquare200@gmail.com>
2025-03-09 08:57:10 +0000

    initial commit with readme update

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "my react-router" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
